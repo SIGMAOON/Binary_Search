cd4ebeee7d0f5649a08ab00e15d6330d1a351bd4
Jiwon Moon <>
2021-06-02 08:42:44 +0000

    solution

--- 350.py
+++ 350.py
@@ -1,2 +1,13 @@
 #Intersection of Two Arrays II
-print(3)
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        # 76ms, 14.3 MB
+        
+        intersection = []
+        
+        for num in nums1:
+            if num in nums2:
+                intersection.append(num)
+                nums2.remove(num)
+        return sorted(intersection)
